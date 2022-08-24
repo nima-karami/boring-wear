4288ba0b00adc69bd0d42af39b92151e23ceaa11
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-08-24 11:12:17 -0400

    adding shop route

---
+++ src/routes/SigninForm.js
@@ -0,0 +1,140 @@
+import * as React from 'react';
+import { useContext } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import GoogleIcon from '@mui/icons-material/Google';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { SingInWithGooglePopup, CreateUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../utils/Firebase/Firebase';
+
+const theme = createTheme();
+
+export default function SignIn() {
+
+    const emailInput = React.useRef(null);
+    const passwordInput = React.useRef(null);
+    
+    const resetFormFields = () => {
+        emailInput.current.value = "";
+        passwordInput.current.value = "";
+    }
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const email = data.get('email');
+        const password = data.get('password');
+
+        try {
+
+            await signInAuthUserWithEmailAndPassword(email, password);
+            // setCurrentUser(user);
+            resetFormFields();
+
+        } catch (error) {
+            if (error.code === 'auth/wrong-password') {
+                alert('cannot sign in: wrong password');
+            } else if (error.code === 'auth/user-not-found'){
+                alert('cannot sign in: user not found');
+            } else {
+                console.log('user sign in failed', error);
+            }
+        }
+    };
+
+    const signInWithGoogle = async () => {
+        await SingInWithGooglePopup();
+        // setCurrentUser(user);
+        
+    }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              inputRef={emailInput}
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              inputRef={passwordInput}
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Typography align="center" variant="body2" > Or </Typography>
+            <Button
+              onClick={signInWithGoogle}
+              fullWidth
+              variant="outlined"
+              sx={{ mt:2 , mb: 2 }}
+            >
+              <GoogleIcon sx={{ mr:1 }} /> Continue With Google
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signUp" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+       
+      </Container>
+    </ThemeProvider>
+  );
+}
---
+++ src/routes/SignupForm.js
@@ -0,0 +1,156 @@
+import * as React from 'react';
+import { useContext } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createAuthUserWithEmailAndPassword, CreateUserDocumentFromAuth } from '../utils/Firebase/Firebase';
+
+
+
+const theme = createTheme();
+
+export default function SignUp() {
+    
+    const firstNameInput = React.useRef(null);
+    const lastNameInput = React.useRef(null);
+    const emailInput = React.useRef(null);
+    const passwordInput = React.useRef(null);
+    
+    const resetFormFields = () => {
+        firstNameInput.current.value = "";
+        lastNameInput.current.value = "";
+        emailInput.current.value = "";
+        passwordInput.current.value = "";
+    }
+  
+    const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const displayName = data.get('firstName') + ' ' + data.get('lastName');
+    const email = data.get('email');
+    const password = data.get('password');
+
+    if (password.length < 8) {
+        alert('Password needs to be longer than 8 characters');
+        return;
+    }
+
+    try {
+        console.log(displayName);
+        const { user } = await createAuthUserWithEmailAndPassword(email, password);
+        await CreateUserDocumentFromAuth(user, {displayName});
+        resetFormFields();
+    } catch (error) {
+        if (error.code === 'auth/email-already-in-use') {
+            alert('cannot create user, email already in use');
+        } else {
+            console.log('user creation failed', error);
+        }
+    }
+    
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  inputRef={firstNameInput}
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  inputRef={lastNameInput}
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  inputRef={emailInput}
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  inputRef={passwordInput}
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/signIn" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
