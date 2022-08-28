d0cca4b9c2745b6f48469a26db7f013960d6f0c7
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-08-28 17:14:39 -0400

    updating shop to show categories map

--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -1,6 +1,7 @@
 import ProductCard from '../components/ProductCard';
 import Container from '@mui/material/Container';
-import { useContext } from 'react';
+import { Fragment, useContext } from 'react';
+import Typography from '@mui/material/Typography';
 
 import { CategoriesContext } from '../context/CategoriesContextProvider';
 
@@ -11,10 +12,18 @@ const Shop = () => {
       <Container maxWidth="false" sx={{ display: 'flex', flexWrap: 'wrap' , padding: '50px' }}>
         {
           Object.keys(categoriesMap).map( (title) => (
-              categoriesMap[title].map(({ id, name, imageUrl, price }) => (
-                <ProductCard key = {id} id = {id} name = {name} imageUrl = {imageUrl} price = {price} />
-              ))
+              <Fragment>
+                <Typography variant='h6' sx={{ display: 'block', margin: 'auto', paddingTop: '25px' }}> {title.toUpperCase()} </Typography>
+
+                <Container maxWidth="false" sx={{ display: 'grid', justifyItems: 'center', gridTemplateColumns: { xs: 'repeat(1, 1fr)' , md: 'repeat(2, 1fr)', lg: 'repeat(5, 1fr)'}, padding: '50px' }}>
+                  { categoriesMap[title].slice(0, 5).map(({ id, name, imageUrl, price }) => (
+                    <ProductCard key = {id} id = {id} name = {name} imageUrl = {imageUrl} price = {price} />
+                  )) }
+                </Container>
+                
+              </Fragment>
           ))
+          
         }
       </Container>
   );
