88ff4448758df5995ec3d34376ea2314861fcdc2
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-09-29 18:44:03 -0400

    adding stripe API

--- src/utils/Stripe/Stripe.js
+++ src/utils/Stripe/Stripe.js
@@ -1,10 +1,7 @@
 import { loadStripe } from '@stripe/stripe-js';
 
-// export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
-export const stripePromise = loadStripe('pk_test_51LnR2rJEjrMBYx3iXbCyR9EPQwQjJDXXWDSKgIW2NVVoEiF5ocnPfNODkToaXU4dSRRUcPWxwkilD9wO6VEFcVzY00nlRHGVb0');
+export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+// export const stripePromise = loadStripe('pk_test_51LnR2rJEjrMBYx3iXbCyR9EPQwQjJDXXWDSKgIW2NVVoEiF5ocnPfNODkToaXU4dSRRUcPWxwkilD9wO6VEFcVzY00nlRHGVb0');
 
-export const stripeOptions = {
-    // passing the client secret obtained from the server
-    clientSecret: 'sk_test_51LnR2rJEjrMBYx3iVmyYtSQGFMVuVfVzHNGIBNVRvvcM8niRLP1bqDAbBCb11re9JL4pCrFXBwdx7aKGtFGMruBA005716csMA'
-}
+
 
